d1c0680449ef12c8e448d656b6a7f508f829246e
suhojo <suhojo@woowahan.com>
2019-03-22 08:47:17 +0900

    first commit

---
+++ src/units/calculate.pas
@@ -0,0 +1,20 @@
+unit calculate;
+
+interface
+
+type
+  TCalculate = class
+    public
+      function Add(A,B :uint32): uint32;
+
+  end;
+implementation
+
+{ TCalculate }
+
+function TCalculate.Add(A, B: uint32): uint32;
+begin
+    Result := A + B;
+end;
+
+end.                                                                            s
