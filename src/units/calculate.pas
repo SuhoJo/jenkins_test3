73d3ce60ee0650d0416053f3dd20435de433bd5b
suhojo <suhojo@woowahan.com>
2019-03-25 20:22:13 +0900

    에러 추가

--- src/units/calculate.pas
+++ src/units/calculate.pas
@@ -14,7 +14,7 @@ implementation
 
 function TCalculate.Add(A, B: uint32): uint32;
 begin
-    Result := A + B;
+    Result := A + Succ(B);
 end;
 
 end.                                                                            s
